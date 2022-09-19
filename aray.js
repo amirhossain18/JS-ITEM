48c09707afe09d5e1fb3659fd768acc5a1e2cd0d
amirhossainbond <67521933+amirhossainbond@users.noreply.github.com>
2022-09-19 23:21:54 +0600

    up

---
+++ aray.js
@@ -0,0 +1,23 @@
+var ages = [20, 14, 50, 40, 60];
+var names= ['aman', 'alom', 'amir'];
+var elements =names['2'];
+var positions =names.indexOf('alom');
+console.log(positions);
+ages[4] = 100,
+console.log(ages);
+// console.log(positions);
+// console.log(elements);
+// console.log(names);
+// array items or length
+console.log(names.length);
+// index ar value
+console.log(names[1]);
+// full array
+console.log(ages);
+// value change
+ages[2]=26;              
+console.log(ages);
+
+// names.push("hasan");
+// console.log(names);
+
---
+++ isoscales.js
@@ -0,0 +1,11 @@
+var sideOne = 10;
+var sideTwo = 11;
+var sideThree = 9;
+
+if (sideOne == sideTwo || sideOne == sideThree || sideTwo == sideThree) {
+    console.log("this is isoscales");
+}
+
+else{
+    console.log("this is not isoscales");
+}
